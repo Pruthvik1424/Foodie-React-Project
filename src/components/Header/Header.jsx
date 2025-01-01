08986b3f62c95c9ed5ca280e933a9a6b674ac525
Pruthvik1424 <pruthvik1459@gmail.com>
2025-01-01 18:05:07 +0530

    Food delivery app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Fresh. Fast. At Your Door.</h2>
+            <p>Choose from desire menu featuring a delectable array of dishes crafted with the finest ingredients and satisfying your craving and elvate your dining experience, one delecious meal at a time </p>
+            <button>View menu</button>
+        </div>  
+    </div>
+  )
+}
+
+export default Header
